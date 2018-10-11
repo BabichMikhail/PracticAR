9ba1ec01cf13cc39aaf8e0d95dc9bf6c195e4861
Mikhail Babich <babich-mv@yandex.ru>
2018-10-11 14:12:07 +1000

    Initial

---
+++ Assets/Resources/Scripts/DragonController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+
+public class DragonController : MonoBehaviour
+{
+	private NavMeshAgent agent;
+	public GameObject bullet;
+	private bool shooted = false;
+	public GameObject createdBullet;
+
+	private void Start ()
+	{
+		agent = gameObject.GetComponent<NavMeshAgent>();
+		agent.SetDestination(transform.position + new Vector3(0, 0, 50f));
+	}
+
+	public void Update()
+	{
+		if (Time.time > 1f && !shooted)
+		{
+			shooted = true;
+			createdBullet = Instantiate(bullet, gameObject.transform);
+		}
+
+		if (createdBullet != null)
+		{
+			createdBullet.transform.position += new Vector3(0, 0, 1.0f) * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/Geometry.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Geometry : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int CountRound;
+
+    public Vector3 CircleCenter;
+    public float Radius;
+    public float Speed;
+
+    private List<GameObject> RoundObjects = new List<GameObject>();
+
+	private void Start ()
+    {
+        float step = 360 / (float)CountRound;
+	    for (int i = 0; i < CountRound; ++i)
+        {
+            var obj = Instantiate(Prefab, gameObject.transform);
+            var position = CircleCenter;
+            position.x += Radius * Mathf.Cos(step * i);
+            position.y += Radius * Mathf.Sin(step * i);
+            obj.transform.position = CircleCenter;
+            RoundObjects.Add(obj);
+        }
+	}
+	
+	private void Update ()
+    {
+		
+	}
+}
