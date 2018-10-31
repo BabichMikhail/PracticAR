c7ea1ef12135cda21f8fa8d28abe080f696d77af
Mikhail Babich <babich-mv@yandex.ru>
2018-11-01 02:28:27 +1000

    Move agent relatively main camera

--- Assets/Resources/Scripts/DragonController.cs
+++ Assets/Resources/Scripts/DragonController.cs
@@ -34,21 +34,28 @@ public class DragonController : MonoBehaviour
 
     private Vector3 HandleMove(string keyCode, Vector3 baseMove)
     {
-        Vector3 move = Vector3.zero;
+        var move = Vector3.zero;
         if (Input.GetKey(keyCode))
-            move = baseMove * Scale;
-            //move = (baseMove.normalized - camera.transform.position.normalized) * Scale;
-        //Debug.Log(move);
+        {
+            // Переводим все координаты в плоскость Oxz и считаем угол, на который нам нужно повернуть вектор "движения".
+            var flatCameraVector = initCameraPosition - initPersonPosition;
+            flatCameraVector.y = 0;
+            // Применение классической матрицы поворота: x' = x * cos(a) - y * sin(a); y' = y * cos(a) + x * sin(a);
+            var angle = Mathf.Deg2Rad * Vector3.Angle(flatCameraVector.normalized, Vector3.forward);
+            move.x = baseMove.x * Mathf.Cos(angle) - baseMove.z * Mathf.Sin(angle);
+            move.z = baseMove.z * Mathf.Cos(angle) + baseMove.x * Mathf.Sin(angle);
+            move *= Scale;
+        }
         return move;
     }
 
 	public void Update()
 	{
-        Vector3 deltaPosition = 
+        var deltaPosition =
             HandleMove("w", new Vector3(1, 0, 0)) +
-            HandleMove("a", new Vector3(0, 0, -1)) +
+            HandleMove("a", new Vector3(0, 0, 1)) +
             HandleMove("s", new Vector3(-1, 0, 0)) +
-            HandleMove("d", new Vector3(0, 0, 1));
+            HandleMove("d", new Vector3(0, 0, -1));
         
         if (Input.GetKey("q"))
         {
